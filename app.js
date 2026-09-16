5946eabbeeadffe1c998aba4fa26f945dde76e59
dariusjmdev <marinescu.dariusjoanito@jgvaldemora.org>
2026-09-16 12:54:39 +0200

    Añadido botón y contador de pulsaciones, el padre de Uriel sigue.

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 console.log("¡Hola DWEC! Mi taller ya funciona.");
+let contador = 0;
+document.getElementById("boton").addEventListener("click", function(){
+    contador++;
+    document.getElementById("Mensaje").textContent = "Botón pulsado " + contador + " veces " ;
+});
