59fb16152ec20879ec79eb9c09153a4c0684cc25
dariusjmdev <marinescu.dariusjoanito@jgvaldemora.org>
2026-09-16 13:22:23 +0200

    Añadidos 3000 estilos de css, botón de modo noche, función para cambiar al modo noche.

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ console.log("¡Hola DWEC! Mi taller ya funciona.");
 let contador = 0;
 const boton = document.getElementById("boton");
 const mensaje = document.getElementById("Mensaje");
+const modoNoche = document.getElementById("modoNoche");
 boton.addEventListener("click", function(){
     contador++;
     mensaje.textContent = "Botón pulsado " + contador + " veces " ;
+});
+modoNoche.addEventListener("click", function(){
+    document.body.classList.toggle("tema-claro");
 });
